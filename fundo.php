868b9fdc2f29ce633414652c27aa3a2a91699706
Alberto Leoncio <40328018+albertoleoncio@users.noreply.github.com>
2022-09-23 09:26:38 -0300

    Update fundo.php
    Atualização para compatibilizar com mudanças no SIAN

--- fundo.php
+++ fundo.php
@@ -1,21 +1,35 @@
 <?php
 
 /* 
+ *
+ * PARAMETROS PARA ATIVAÇÃO QUE DEVEM SER SUBSTITUIDOS
+ * 
+ * $login = E-mail cadastrado no SIAN
+ * $senha = Senha cadastrada no SIAN
+ * $local = Diretório onde este arquivo esteja disponível. Altere caso necessário.
+ * 
+ */
 
-PARAMETROS PARA ATIVAÇÃO QUE DEVEM SER SUBSTITUIDOS
+$login = "XXX";
+$senha = "YYY";
+$local = "http://localhost:8080/";
+$colec = $_GET["colecao"];
+$pagin = $_GET["pag"];
 
-LOGIN = E-mail cadastrado no SIAN
-SENHA = Senha cadastrada no SIAN
-ENDEREÇO = Diretório onde este arquivo esteja disponível
+/* 
+ * 
+ * SCRIPT
+ *
+ */
 
-*/
 
+//Exibir erros durante a execução
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-//Cookie de login
-$ch1 = curl_init( "https://sian.an.gov.br/sianex/consulta/login.asp" );
+//Capturar cookie para login
+$ch1 = curl_init( "https://sian.an.gov.br/sianex/consulta/login-novo-com-cadastro.asp" );
 curl_setopt( $ch1, CURLOPT_RETURNTRANSFER, true );
 curl_setopt( $ch1, CURLOPT_HTTPHEADER, ['Referer' => 'https://sian.an.gov.br/sianex/consulta/resultado_pesquisa_new.asp']);
 curl_setopt( $ch1, CURLOPT_COOKIEJAR, "cookie.txt" );
@@ -23,22 +37,21 @@ curl_setopt( $ch1, CURLOPT_COOKIEFILE, "cookie.txt" );
 $output_cookie = curl_exec( $ch1 );
 curl_close( $ch1 );
 
-
-//Login
+//Realiza login no servidor
 $params2 = [
-		"login" 		=> "LOGIN",
-		"senha" 		=> "SENHA",
-		"caminho" 			=> "",
-		"destinatario" 		=> "",
-		"fonecontato" 		=> "",
-		"assinaturacontato" => ""
-	];
+        "login"             => $login,
+        "senha"             => $senha,
+        "caminho"           => "",
+        "destinatario"      => "",
+        "fonecontato"       => "",
+        "assinaturacontato" => ""
+];
 $ch2 = curl_init();
 curl_setopt( $ch2, CURLOPT_URL, "https://sian.an.gov.br/sianex/consulta/Verifica_Login.asp" );
 curl_setopt( $ch2, CURLOPT_POST, true );
 curl_setopt( $ch2, CURLOPT_POSTFIELDS, http_build_query( $params2 ) );
 curl_setopt( $ch2, CURLOPT_RETURNTRANSFER, true );
-curl_setopt( $ch2, CURLOPT_HTTPHEADER, ['Referer' => 'https://sian.an.gov.br/sianex/consulta/login.asp']);
+curl_setopt( $ch2, CURLOPT_HTTPHEADER, ['Referer' => 'https://sian.an.gov.br/sianex/consulta/login-novo-com-cadastro.asp']);
 curl_setopt( $ch2, CURLOPT_COOKIEJAR, "cookie.txt" );
 curl_setopt( $ch2, CURLOPT_COOKIEFILE, "cookie.txt" );
 curl_setopt( $ch2, CURLOPT_FOLLOWLOCATION, true);
@@ -47,21 +60,21 @@ curl_close( $ch2 );
 
 
 //Busca coleção
-if (!isset($_GET["pag"])) $_GET["pag"] = 1;
-$params3 = [		
-		"input_pesqfundocolecao" 	=> $_GET["colecao"],
-		"data_inicio" 				=> "",
-		"data_fim" 					=> "",
-		"input_pesqnotacao" 		=> "",
-		"nivel" 					=> "",
-		"checked" 					=> "",
-		"txt_pesquisa" 				=> "",
-		"v_pesquisa" 				=> "",
-		"v_fundo_colecao" 			=> $_GET["colecao"],
-		"v_ordem" 					=> "Relevancia",
-		"pesquisa" 					=> ""
-	];
-$ch3 = curl_init( "https://sian.an.gov.br/sianex/consulta/resultado_pesquisa_new.asp?v_pesquisa=&v_fundo_colecao=".$_GET["colecao"]."&Pages=".$_GET["pag"] );
+if (!isset($pagin)) $pagin = 1;
+$params3 = [        
+        "input_pesqfundocolecao"    => $colec,
+        "data_inicio"               => "",
+        "data_fim"                  => "",
+        "input_pesqnotacao"         => "",
+        "nivel"                     => "",
+        "checked"                   => "",
+        "txt_pesquisa"              => "",
+        "v_pesquisa"                => "",
+        "v_fundo_colecao"           => $colec,
+        "v_ordem"                   => "Relevancia",
+        "pesquisa"                  => ""
+];
+$ch3 = curl_init( "https://sian.an.gov.br/sianex/consulta/resultado_pesquisa_new.asp?v_pesquisa=&v_fundo_colecao=".$colec."&Pages=".$pagin );
 curl_setopt( $ch3, CURLOPT_RETURNTRANSFER, true );
 curl_setopt( $ch3, CURLOPT_POST, true );
 curl_setopt( $ch3, CURLOPT_POSTFIELDS, http_build_query( $params3 ) );
@@ -76,24 +89,32 @@ curl_close( $ch3 );
 //Com sucesso, retorna lista de links para outras páginas e links de arquivos para download.
 if ($output == FALSE) {
 
-	
-	sleep(15);
-	http_response_code(303);
-	header("Location: https://ENDEREÇO/fundo.php?colecao=".$_GET["colecao"]."&pag=".$_GET["pag"]."&time=".time());
+    
+    sleep(15);
+    http_response_code(303);
+    header("Location: ".$local."/fundo.php?colecao=".$colec."&pag=".$pagin."&time=".time());
 
 } else {
 
-	echo("<pre>");
-	preg_match_all('/var TotalPag = (\d*);/', $output, $pages);
-	for ($p=1; $p <= $pages['1']['0']; $p++) { 
-		echo("<a href='fundo.php?colecao=".$_GET["colecao"]."&pag=".$p."'>Página ".$p."</a>\n");
-	}
-	echo("<hr>");
-  
-	  preg_match_all('/download do arquivo" class="help_pesquisa" onClick="javascript:fjs_Link_download\(\'([^\']*)\',\'([^\']*)\',\'([^\']*)\'[\s\S]*?\n<input type = hidden id="in_(\d*)/', $output, $files);
+    //Abre html
+    echo("<pre>");
 
-	for ($i=0; $i < count($files['0']); $i++) { 
-		echo("<a href='https://sian.an.gov.br/sianex/consulta/download.asp?arquivo=".$files[1][$i]."&NomeArquivo=".$files[2][$i]."&apresentacao=".$files[3][$i]."' download='".$files[2][$i]."'>".$files[2][$i]."</a>\n<a href='dossie.php?id=".$files[4][$i]."'>".$files[4][$i]."</a>\n\n\n");
-	}
+    //Captura número de páginas no fundo e cria loop para exibição da lista
+    preg_match_all('/var TotalPag = (\d*);/', $output, $pages);
+    for ($p=1; $p <= $pages['1']['0']; $p++) { 
+        echo("<a href='fundo.php?colecao=".$colec."&pag=".$p."'>Página ".$p."</a>\n");
+    }
+
+    //Linha para diferenciar seções
+    echo("<hr>");
+  
+    //Captura arquivos para download na página atual e cria loop para exibição da lista
+    preg_match_all('/download do arquivo" class="help_pesquisa" onClick="javascript:fjs_Link_download\(\'([^\']*)\',\'([^\']*)\',\'([^\']*)\'[\s\S]*?\n<input type = hidden id="in_(\d*)/', $output, $files);
+    for ($i=0; $i < count($files['0']); $i++) { 
+        echo("<a href='https://sian.an.gov.br/sianex/consulta/download.asp?arquivo=".$files[1][$i]."&NomeArquivo=".$files[2][$i]."&apresentacao=".$files[3][$i]."' download='".$files[2][$i]."'>".$files[2][$i]."</a>\n<a href='dossie.php?id=".$files[4][$i]."'>".$files[4][$i]."</a>\n\n\n");
+    }
+
+    //Fecha html
+    echo("</pre>");
 
 }
