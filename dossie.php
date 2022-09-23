7d2bea9e07979777eb561d0a06f03f7e00312bb2
Alberto Leoncio <40328018+albertoleoncio@users.noreply.github.com>
2022-09-23 09:57:41 -0300

    Update dossie.php

--- dossie.php
+++ dossie.php
@@ -1,40 +1,57 @@
 <?php
 
 /* 
+ *
+ * PARAMETROS PARA ATIVAÇÃO QUE DEVEM SER SUBSTITUIDOS
+ * 
+ * $login = E-mail cadastrado no SIAN
+ * $senha = Senha cadastrada no SIAN
+ * $local = Diretório onde este arquivo esteja disponível. Altere caso necessário.
+ * 
+ */
 
-PARAMETROS PARA ATIVAÇÃO QUE DEVEM SER SUBSTITUIDOS
+$login = "XXX";
+$senha = "YYY";
+$local = "http://localhost:8080/";
+$id    = $_GET["id"];
 
-LOGIN = E-mail cadastrado no SIAN
-SENHA = Senha cadastrada no SIAN
-ENDEREÇO = Diretório onde este arquivo esteja disponível
+/* 
+ * 
+ * SCRIPT
+ *
+ */
 
-*/
 
-//Cookie de login
-$ch1 = curl_init( "https://sian.an.gov.br/sianex/consulta/login.asp" );
+//Exibir erros durante a execução
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+
+//Capturar cookie para login
+$ch1 = curl_init( "https://sian.an.gov.br/sianex/consulta/login-novo-com-cadastro.asp" );
 curl_setopt( $ch1, CURLOPT_RETURNTRANSFER, true );
-curl_setopt( $ch1, CURLOPT_HTTPHEADER, array('Referer: https://sian.an.gov.br/sianex/consulta/resultado_pesquisa_new.asp') );
+curl_setopt( $ch1, CURLOPT_HTTPHEADER, ['Referer' => 'https://sian.an.gov.br/sianex/consulta/resultado_pesquisa_new.asp']);
 curl_setopt( $ch1, CURLOPT_COOKIEJAR, "cookie.txt" );
 curl_setopt( $ch1, CURLOPT_COOKIEFILE, "cookie.txt" );
 $output_cookie = curl_exec( $ch1 );
 curl_close( $ch1 );
 
-
-//Login
+//Realiza login no servidor
 $params2 = [
-		"login" 		=> "LOGIN",
-		"senha" 		=> "SENHA",
-		"caminho" 			=> "",
-		"destinatario" 		=> "",
-		"fonecontato" 		=> "",
-		"assinaturacontato" => ""
-	];
+        "login"             => $login,
+        "senha"             => $senha,
+        "caminho"           => "",
+        "destinatario"      => "",
+        "fonecontato"       => "",
+        "assinaturacontato" => ""
+];
 $ch2 = curl_init();
 curl_setopt( $ch2, CURLOPT_URL, "https://sian.an.gov.br/sianex/consulta/Verifica_Login.asp" );
 curl_setopt( $ch2, CURLOPT_POST, true );
 curl_setopt( $ch2, CURLOPT_POSTFIELDS, http_build_query( $params2 ) );
 curl_setopt( $ch2, CURLOPT_RETURNTRANSFER, true );
-curl_setopt( $ch2, CURLOPT_HTTPHEADER, array('Referer: https://sian.an.gov.br/sianex/consulta/login.asp') );
+curl_setopt( $ch2, CURLOPT_HTTPHEADER, ['Referer' => 'https://sian.an.gov.br/sianex/consulta/login-novo-com-cadastro.asp']);
 curl_setopt( $ch2, CURLOPT_COOKIEJAR, "cookie.txt" );
 curl_setopt( $ch2, CURLOPT_COOKIEFILE, "cookie.txt" );
 curl_setopt( $ch2, CURLOPT_FOLLOWLOCATION, true);
@@ -43,8 +60,8 @@ curl_close( $ch2 );
 
 
 //Busca dossie
-$params3 = [		
-		"ID" 	=> $_GET["id"]
+$params3 = [        
+        "ID"    => $id
 ];
 $ch3 = curl_init();
 curl_setopt( $ch3, CURLOPT_URL, "https://sian.an.gov.br/sianex/consulta/Pesquisa_Livre_Controle.asp" );
@@ -61,17 +78,17 @@ curl_close( $ch3 );
 //Sem sucesso, aguarda 15 segundos e atualiza página. 
 //Com sucesso, retorna conteúdo da página e força download utilizando o código de referência como título.
 if ($output == FALSE) {
-	sleep(15);
-	http_response_code(303);
-	header("Location: https://ENDEREÇO/dossie.php?id=".$_GET["id"]."&time=".time());
+    sleep(15);
+    http_response_code(303);
+    header("Location: ".$local."/dossie.php?id=".$id."&time=".time());
 } else {
-	preg_match_all('/<label style=\'color:red;font-size:12px;\'><i>([^<]*)<\/i>/', $output, $filename);
-	if (!isset($filename['1']['0'])) $filename['1']['0'] = $_GET["id"];
+    preg_match_all('/<label style=\'color:red;font-size:12px;\'><i>([^<]*)<\/i>/', $output, $filename);
+    if (!isset($filename['1']['0'])) $filename['1']['0'] = $id;
 
-	header('Content-Disposition: attachment; filename="'.trim($filename['1']['0'], " -").'.html"');
+    header('Content-Disposition: attachment; filename="'.trim($filename['1']['0'], " -").'.html"');
     header('Content-Description: File Transfer');
-	header("Cache-Control: no-cache, must-revalidate");
-	header("Expires: 0");
+    header("Cache-Control: no-cache, must-revalidate");
+    header("Expires: 0");
 
-	echo($output);
+    echo($output);
 }
